4e108982b86e802b9bc7ab4fcc673b989a57a495
Duy <trankhanhduy8599@gmail.com>
2020-05-16 23:30:50 +0700

    Glitch

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URL = "mongodb://localhost/express"
+const URL = "mongodb+srv://duy-nirvana:" + process.env.mongodb_password + "@cluster0-so3bl.mongodb.net/test?retryWrites=true&w=majority"
 
 const connect = async () => {
   await mongoose.connect(URL,
